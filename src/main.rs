8543661b6f017483e7c6a39b12b0296c5abe2391
nmiyamoto <scotsdowneroad@gmail.com>
2021-04-19 14:42:41 +0900

    impl sht3x sensor

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,9 @@ use wio::hal::clock::GenericClockController;
 use wio::hal::delay::Delay;
 use wio::prelude::*;
 
+use wio::hal::sercom::{I2CMaster3, Sercom3Pad0, Sercom3Pad1};
+use wio::hal::gpio::{Pa16, Pa17, PfD};
+
 #[entry]
 fn main() -> ! {
     let core = CorePeripherals::take().unwrap();
@@ -55,8 +58,81 @@ fn main() -> ! {
     t.write_str(" Hello!\n");
     t.write_str(" This is Wio sauna monitor from rust\n");
 
+    //Initialize i2c
+    let user_i2c = sets.i2c.init(
+        &mut clocks,
+        peripherals.SERCOM3,
+        &mut peripherals.MCLK,
+        &mut sets.port,
+    );
 
-    loop{
+    //Initialize SHT sensor
+    let device_address = 0x44u8;
+    let mut sht3 = SHT3X::new(user_i2c, device_address);
+    let mut s: [char; 3] = [0 as char; 3];
+
+    loop {
+        // measure data
+        sht3.measure();
+        
+        // show temp and humidity
+        t.write_str(" temp: ");
+        u8to_str(sht3.get_temp() as u8, &mut s);
+        for i in 0..3{ t.write_character(s[i]); }
+        t.write_str(" humid: ");
+        u8to_str(sht3.get_humid() as u8, &mut s);
+        for i in 0..3{ t.write_character(s[i]); }
+
+        t.write_str("\n");
+        //wait 1[s]
+        delay.delay_ms(1000u32);
+    }
+}
+
+fn u8to_str(numu8 : u8, s: &mut [char;3]){
+    let mut num = numu8;
+    let mut n = 100u8;
+    for i in 0..3{
+        s[i] = (0x30u8 + num/n) as char;
+        num = num%n;
+        n=n/10;
+    }
+}
+
+type I2ctype = I2CMaster3<Sercom3Pad0<Pa17<PfD>>, Sercom3Pad1<Pa16<PfD>>>;
+struct SHT3X{
+    address : u8,
+    i2c : I2ctype,
+    c_temp: f32,
+    humid : f32,
+}
+
+impl SHT3X{
+    pub fn new(i2cm:I2ctype, addr: u8) -> Self {
+        Self {
+            address: addr,
+            i2c: i2cm,
+            c_temp: 0.0,
+            humid : 0.0,
+        }
+    }
+    pub fn measure(&mut self){
+        let wdata: [u8; 2] = [0x2C, 0x06];
+        let _ret = self.i2c.write(self.address, &wdata);
+
+        let mut rdata: [u8; 6] = [0,0,0,0,0,0];
+        let _ret = self.i2c.read(self.address, &mut rdata);
+
+        self.c_temp = (((rdata[0] as f32 * 256.0) + rdata[1] as f32) * 175.0) / 65535.0 - 45.0;
+        self.humid  = (((rdata[3] as f32 * 256.0) + rdata[4] as f32) * 100.0) / 65535.0;
+    }
+
+    pub fn get_temp(&self) -> f32{
+        self.c_temp
+    }
+
+    pub fn get_humid(&self) -> f32{
+        self.humid
     }
 }
 
