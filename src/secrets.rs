0f05a28d5e0e20d829b34fca1e4526a5e9939e24
n-miyamoto <scotsdowneroad@gmail.com>
2021-05-09 12:56:45 +0900

    add dummy const params

--- src/secrets.rs
+++ src/secrets.rs
@@ -2,3 +2,14 @@ pub mod wifi{
     pub const SSID: &str = "ssid";
     pub const PASS: &str = "password";
 }
+
+pub mod ambient{
+    pub const CHANNEL_ID: u32 = 12345;
+    pub const WRITE_KEY : &str = "123456789";
+    pub const IP: u32 = 0x3BCE4136; //54.65.206.59
+    pub const PORT :u16 = 0x5000; //80
+}
+
+pub mod params{
+    pub const INTERVAL_MS: u32 = 5000;
+}
