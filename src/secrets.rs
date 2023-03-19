0ef0abdc7c82b10f3ba706b4d7ec2a984ab0a50a
n-miyamoto <scotsdowneroad@gmail.com>
2023-03-19 17:36:46 +0900

    Update secrets template

--- src/secrets.rs
+++ src/secrets.rs
@@ -1,6 +1,6 @@
 pub mod wifi{
-    pub const SSID: &str = "ssid";
-    pub const PASS: &str = "password";
+    pub const SSID: &str = "<ssid>";
+    pub const PASS: &str = "<password>";
 }
 
 pub mod ambient{
@@ -8,6 +8,7 @@ pub mod ambient{
     pub const WRITE_KEY : &str = "123456789";
     pub const IP: u32 = 0x3BCE4136; //54.65.206.59
     pub const PORT :u16 = 0x5000; //80
+    pub const BASE_URI: &str = "/api/v2/channels";
 }
 
 pub mod params{
